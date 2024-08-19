8d94bced27e27beb40948758e8ca63ed8add7b02
Bryant <bciputra@student.unimelb.edu.au>
2024-08-18 18:00:37 -0700

    feat: initial commit

---
+++ components/main/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeroContent } from "../sub/HeroContent";
+
+export const Hero = () => {
+	return (
+		<div className="relative flex flex-col h-full w-full">
+			<HeroContent />
+		</div>
+	);
+};
