81bff00d3ab45dd024a36672e654bcbd46f7f331
Bryant <bciputra@student.unimelb.edu.au>
2024-08-25 16:15:27 -0700

    chore: update README

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import { Hero } from "@/components/ui/Hero";
 import Skills from "@/components/ui/Skills";
 
 export default function Home() {
-  return (
-    <main className='h-full w-full '>
-      <div className='flex flex-col gap-y-72'>
-        <Hero />
-        <AboutMe />
-        <Experience />
-        <Education />
-        <Skills />
-      </div>
-    </main>
-  );
+	return (
+		<main className="h-full w-full ">
+			<div className="flex flex-col gap-y-72">
+				<Hero />
+				<AboutMe />
+				<Experience />
+				<Education />
+				<Skills />
+			</div>
+		</main>
+	);
 }
